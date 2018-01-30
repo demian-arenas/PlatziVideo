bbc91e8a0673457292a26d8cbcc634e439dd61da
Demian Arenas <arenasdemian99@gmail.com>
2018-01-30 00:19:30 -0600

    [add] simple hello world with React

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app')
+const holaMundo = <h1>Hola Estudiante!</h1>
+
+ReactDOM.render(holaMundo, app)
