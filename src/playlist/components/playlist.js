d51234ba51269239488637434d9a75cc8e2676e4
Demian Arenas <arenasdemian99@gmail.com>
2018-01-31 05:28:10 -0600

    [update] modify Playlist component to implement with Category

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -3,11 +3,9 @@ import Media from "./media.js";
 import "./playlist.css";
 
 const Playlist = (props) => {
-    const playlist = props.data.categories[0].playlist;
-    console.log(props.data);
     return (
       <div className="Playlist">
-        {playlist.map(item => {
+        {props.playlist.map(item => {
           return <Media {...item} key={item.id} />;
         })}
       </div>
