22459ac63bfb2a10a731e1beab736b79f4fcd4a2
Demian Arenas <arenasdemian99@gmail.com>
2018-01-31 04:52:20 -0600

    [update] convert class component to function component

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Media from "./media.js";
 import "./playlist.css";
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist;
-    console.log(this.props.data);
+const Playlist = (props) => {
+    const playlist = props.data.categories[0].playlist;
+    console.log(props.data);
     return (
       <div className="Playlist">
         {playlist.map(item => {
@@ -13,7 +12,6 @@ class Playlist extends Component {
         })}
       </div>
     );
-  }
 }
 
 export default Playlist;
