ba4680ac619dcec0f808fa37ce02bea44ffecc23
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 20:16:43 -0600

    [update] implement onLoadedMetadata

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -3,6 +3,8 @@ import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
 import PlayPause from '../components/play-pause'
+import Timer from '../components/timer'
+import Controls from '../components/video-player-controls'
 
 class VideoPlayer extends Component {
   state = {
@@ -20,15 +22,28 @@ class VideoPlayer extends Component {
     })
   }
 
+  handleLoadedMetadata = event => {
+    this.video = event.target
+    this.setState({
+      duration: this.video.duration
+    })
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title title='Esto es un video chido!' />
-        <PlayPause
-          pause={this.state.pause}
-          handleClick={this.togglePlay}
-        />
+        <Controls>
+          <PlayPause
+            pause={this.state.pause}
+            handleClick={this.togglePlay}
+          />
+          <Timer
+            duration={this.state.duration}
+          />
+        </Controls>
         <Video
+          handleLoadedMetadata={this.handleLoadedMetadata}
           pause={this.state.pause}
           autoplay={this.props.autoplay}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
