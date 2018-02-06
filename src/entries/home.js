2a03e5fb3fce9df6563184b1b4714e1963e4cb11
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 23:20:22 -0600

    [update] set redux store

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,21 @@ import React from "react";
 import { render } from "react-dom";
 import Home from "../pages/containers/home";
 import data from '../api.json'
+import  { createStore } from 'redux'
+
+const initialState = {
+  data: {
+    ...data
+  }
+};
+
+const store = createStore(
+  state => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById("home-container");
 
