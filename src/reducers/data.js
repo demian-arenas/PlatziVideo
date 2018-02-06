3899e9968b5d92a2d273a181ac82ba182c60fb83
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 23:51:39 -0600

    [add] created data reducer

---
+++ src/reducers/data.js
@@ -0,0 +1,12 @@
+const data = (state, action) => {
+  switch (action.type) {
+    case 'something': {
+      return state
+    }
+    break;
+    default:
+      return state
+  }
+}
+
+export default data
