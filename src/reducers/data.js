8b6df6eb0b797740df6b61bd75ea86007f2a2d4a
Demian Arenas <arenasdemian99@gmail.com>
2018-02-06 00:58:31 -0600

    [update] filtering search data

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,9 +1,22 @@
-const data = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_VIDEO":
-      {
-        return state;
+const data = (state, { type, payload}) => {
+  switch (type) {
+    case "SEARCH_VIDEO": {
+      const results = state.data.categories
+      .map(category => category.playlist.filter(item => {
+        const author = item.author.toLowerCase();
+        const query = payload.query.toLowerCase();
+        return author.includes(query)
+      }))
+      .filter(category => {
+        if (category.length > 0) return category
+      })
+      
+
+      return {
+        ...state,
+        search: results
       }
+    }
       break;
     default:
       return state;
