988f85e36edc5f55910f75564482e6ac16e2ac4d
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 18:36:20 -0600

    [update] implement handleInputChange and handleSubmit on Search component

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './search.css'
 const Search = (props) => (
-  <form className='Search'>
+  <form
+    className='Search'
+    onSubmit={props.handleSubmit}
+  >
     <input
+      ref={props.setRef}
       className='Search-input'
       placeholder='Busca tu video favorito'
       type='text'
+      value={props.value}
+      onChange={props.handleChange}
     />
 	</form>
 )
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 import Search from '../components/search'
 
 class SearchContainer extends Component {
+  state = {
+    value: 'Luis Fonsi'
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.input.value)
+  }
+
+  setInputRef = element => {
+    this.input = element
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      value: event.target.value.replace(' ', '-')
+    })
+  }
   render() {
     return (
-      <Search/>
+      <Search
+        value={this.state.value}
+        handleChange={this.handleInputChange}
+        setRef={this.setInputRef}
+        handleSubmit={this.handleSubmit}
+      />
     )
   }
 }
