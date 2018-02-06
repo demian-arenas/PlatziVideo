3d60bdcb884817c4c1c3582e08cacb7057b5b9f4
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 19:26:48 -0600

    [update] implement PlayPause with togglePlay method

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true
+  }
+  togglePlay = (event) => {
+    this.setState((prevState, props) => {
+      return {pause: !prevState.pause}
+   })
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title title='Esto es un video chido!' />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
+        />
         <Video
           autoplay={true}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
