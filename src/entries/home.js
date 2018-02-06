fdf7a950962825780f9653bf0044c717c6386a34
Demian Arenas <arenasdemian99@gmail.com>
2018-02-06 04:42:03 -0600

    [update] implement redux-devtools-extension and redux-logger

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,21 +5,14 @@ import data from '../schemas'
 import  { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Map as map } from 'immutable'
-
-const logger = ({ getState, dispatch}) => next => action => {
-  console.log("estado anterior:", getState().toJS());
-  console.log("enviando acción:", action);
-  const value = next(action);
-  console.log("nuevo estado   :", getState().toJS());
-  return value;
-}
 
 const store = createStore(
   reducer,
   map(),
-  applyMiddleware(logger)
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(logger))
 );
 
 const homeContainer = document.getElementById("home-container");
