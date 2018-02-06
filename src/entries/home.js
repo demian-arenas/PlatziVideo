0e78744c3d90a01df518d0a0ea04614274d2d5ca
Demian Arenas <arenasdemian99@gmail.com>
2018-02-06 01:51:05 -0600

    [add] normalized schemas

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import Home from "../pages/containers/home";
 import data from '../api.json'
+import normalizedData from '../schemas'
 import  { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/data'
@@ -12,6 +13,8 @@ const initialState = {
   },
   search: []
 };
+
+console.log(normalizedData)
 
 const store = createStore(
   reducer,
