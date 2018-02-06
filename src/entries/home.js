8ea62f92362d2d49498c9e6b1dee82cd33ceb3af
Demian Arenas <arenasdemian99@gmail.com>
2018-02-06 04:32:37 -0600

    [update] created simple middleware

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,15 +2,24 @@ import React from "react"
 import { render } from "react-dom"
 import Home from "../pages/containers/home"
 import data from '../schemas'
-import  { createStore } from 'redux'
+import  { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers'
 import { Map as map } from 'immutable'
 
+const logger = ({ getState, dispatch}) => next => action => {
+  console.log("estado anterior:", getState().toJS());
+  console.log("enviando acción:", action);
+  const value = next(action);
+  console.log("nuevo estado   :", getState().toJS());
+  return value;
+}
+
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const homeContainer = document.getElementById("home-container");
