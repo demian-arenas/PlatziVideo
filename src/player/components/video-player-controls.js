f1d532bd1dacee54e8347567c8797033dbb86e8c
Demian Arenas <arenasdemian99@gmail.com>
2018-02-05 20:16:17 -0600

    [add] created VideoPlayerControls component

---
+++ src/player/components/video-player-controls.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./video-player-controls.css";
+
+const  VideoPlayerControls= (props) => (
+  <div class="VideoPlayerControls">{props.children}</div>
+)
+
+export default VideoPlayerControls;
