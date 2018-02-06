413cd063461f49cad58c6911ff6fc8c014133e69
Demian Arenas <arenasdemian99@gmail.com>
2018-02-06 03:32:14 -0600

    [update] pass Search feature to use with immutable

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,24 +4,13 @@ import { fromJS } from "immutable";
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: [],
+  search: ''
 })
 
 const data = (state = initialState, { type, payload}) => {
   switch (type) {
     case "SEARCH_VIDEO": {
-      const results = []
-
-      if (payload.query) {
-        state.data.categories
-        .map(category => category.playlist.filter(item => item.author.toLowerCase().includes(payload.query.toLowerCase()) && results.push(item)
-        ))
-      }
-
-      return {
-        ...state,
-        search: results
-      }
+      return state.set('search', payload.query)
     }
       break;
     default:
